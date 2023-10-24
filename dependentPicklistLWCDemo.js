38ead5b0a6890fa39e594df197b5c477bf5ec79a
Logesh Matheswaran <61358687+LOGESH2107914@users.noreply.github.com>
2023-10-24 20:17:57 +0530

    Update dependentPicklistLWCDemo.js
    Removed commented lines in the JS file.

--- dependentPicklistLWCDemo.js
+++ dependentPicklistLWCDemo.js
@@ -22,12 +22,10 @@ export default class DependentPicklistLWCDemo extends LightningElement {
     }
 
     @wire(getPicklistValues, {recordTypeId: "012000000000000AAA", fieldApiName: SubRegion_FIELD })
-    //subregionPicklistData;
     SubregionPicklistData({data,error}){
         if(data){
             console.log('Sub Region Picklist Daata::',data);
             this.regionSubPicklistData = data;
-
         }
         else if(error){
             console.error(error);
@@ -37,7 +35,6 @@ export default class DependentPicklistLWCDemo extends LightningElement {
     handleRegionChange(event){
         console.log('Entered handleRegionChange');
         let key = this.regionSubPicklistData.controllerValues[event.target.value];
-//this.countyOptions = this.countryFieldData.values.filter(opt => opt.validFor.includes(key));
         this.finalregionSubPicklistData = this.regionSubPicklistData.values.filter(opt => opt.validFor.includes(key));
         console.log('Dependent Country Picklists Values:::',this.finalregionSubPicklistData);
     }
